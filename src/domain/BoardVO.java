a7db947a055e55271c6cdacec6353a5494e125eb
kim tae young <kty6307@naver.com>
2019-09-30 15:39:08 +0900

    BoardVo.java 추가

---
+++ src/domain/BoardVO.java
@@ -0,0 +1,19 @@
+package domain;
+
+import java.util.Date;
+
+import lombok.Data;
+@Data
+public class BoardVO {
+	private int bno;
+	private String name;
+	private String password;
+	private String title;
+	private String content;
+	private String attach;
+	private int re_ref;
+	private int re_lev;
+	private int re_seq;
+	private int readcount;
+	private Date regdate;
+}
